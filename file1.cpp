570061bf33a6abb79484670dbbdd172f1cb31aa8
fadilsyy <fadilasytar@gmail.com>
2025-05-07 09:26:33 +0700

    file1

--- file1.cpp
+++ file1.cpp
@@ -6,4 +6,9 @@ class Mahasiswa{
     int nim;
     string nama;
     float nilai;
+    void PrintData(){
+        cout << "Nama : " << nama << endl;
+        cout << "NIM : " << nim << endl ;
+        cout << "nilai : " << nilai << endl ;
+    }
 };
